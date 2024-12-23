0c46d8fda1ca91dff488e78709c93721ffdde69a
Aragorn <110131167+Clemont0@users.noreply.github.com>
2024-12-22 20:58:11 -0500

    Add files via upload

---
+++ prototype3/main/java/org/example/Entraves.java
@@ -0,0 +1,63 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Entraves {
+
+    @JsonProperty("id_request")
+    private String idRequest;
+
+    @JsonProperty("streetid")
+    private String streetId;
+
+    @JsonProperty("shortname")
+    private String shortName;
+
+    @JsonProperty("streetimpacttype")
+    private String streetImpactType;
+
+
+    public Entraves() {
+
+    }
+
+
+    public Entraves(String idRequest, String streetId, String shortName, String streetImpactType) {
+        this.idRequest = idRequest;
+        this.streetId = streetId;
+        this.shortName = shortName;
+        this.streetImpactType = streetImpactType;
+    }
+
+    public String getIdRequest() {
+        return idRequest;
+    }
+
+    public void setIdRequest(String idRequest) {
+        this.idRequest = idRequest;
+    }
+
+    public String getStreetId() {
+        return streetId;
+    }
+
+    public void setStreetId(String streetId) {
+        this.streetId = streetId;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getStreetImpactType() {
+        return streetImpactType;
+    }
+
+    public void setStreetImpactType(String streetImpactType) {
+        this.streetImpactType = streetImpactType;
+    }
+}
---
+++ prototype3/main/java/org/example/Quartiers.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Quartiers {
+    private final ArrayList<String> quartiers;
+
+    public Quartiers() {
+        this.quartiers = makeQuartiers();
+    }
+
+    public ArrayList<String> getQuartiers() {return quartiers;}
+
+    private static ArrayList<String> makeQuartiers() {
+        ArrayList<String> qs = new ArrayList<>();
+        qs.add("Ahuntsic-Cartierville");
+        qs.add("Anjou");
+        qs.add("Côte-des-Neiges-Notre-Dame-de-Grâce");
+        qs.add("Lachine");
+        qs.add("Lasalle");
+        qs.add("Le Plateau-Mont-Royal");
+        qs.add("Le Sud-Ouest");
+        qs.add("L'Île-Bizard-Sainte-Geneviève");
+        qs.add("Mercier-Hochelaga-Maisonneuve");
+        qs.add("Montréal-Nord");
+        qs.add("Outremont");
+        qs.add("Pierrefonds-Roxboro");
+        qs.add("Rivière-des-Prairies-Pointe-aux-Trembles");
+        qs.add("Rosemont-La Petite-Patrie");
+        qs.add("Saint-Laurent");
+        qs.add("Saint-Léonard");
+        qs.add("Verdun");
+        qs.add("Ville-Marie");
+        qs.add("Villeray-Saint-Michel-Parc-Extension");
+        return qs;
+    }
+
+    public static String getQuartierFromCP(String codePostal) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/codesPostaux.csv"));
+        String line;
+        String quartier = "";
+        String areaCode = codePostal.toUpperCase().substring(0, 3);
+        while ((line = reader.readLine()) != null) {
+            String[] tmp = line.split(",");
+            if (areaCode.equals(tmp[0])) {
+                quartier = tmp[1];
+                break;
+            }
+        }
+        return quartier;
+    }
+}
